96022e909e52274f8293127cf0461a59c441bf35
Peter <schmidt.multimedia@gmail.com>
2021-11-29 12:28:28 +0100

    EventListener / Test

--- events.js
+++ events.js
@@ -1,10 +1,26 @@
 
 /********  Variablen **********/
 
+const btn = document.getElementById("trigBtn");
+// const btn = document.body.children[0];
 
 
 /********  Event-Listener **********/
+btn.addEventListener("click",onClick);
+btn.addEventListener("mouseenter",onRollIn);
+btn.addEventListener("mouseleave",onRollOut);
 
+function onClick() {
+   output("clicked");
+}
+
+function onRollIn() {
+    output("rolled in");
+ }
+
+ function onRollOut() {
+    output("rolled out");
+ }
 
 
 /********  Business-Logic | Toggle **********/
@@ -13,7 +29,7 @@
 
 /********  Änderung in View-Schicht **********/
 
-/
+
 
 /* Tools */
 function output(outputData) {
